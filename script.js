81051fcb3ce1585a02703351b99bd4edbaf828d7
acesavvy101 <gracielladeronne07@gmail.com>
2026-06-27 17:01:51 +0800

    feat: add initial game round logic, winning conditions

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function Gameboard () {
     //instead of pushing into gameboard, we should already have an array with 9 spots, and we need to replace a certain ele in the array (direct indexing)
     let gameboard = ['','','','','','','','',''];
 
-    return { 
+    return { //only returned variables are accessible outside the func
         add: (label, position) => { 
         //disable override, check if the position is empty
           if (gameboard[position]==='') {
@@ -35,29 +35,59 @@ Gameboard1.add('O',4);
 console.log(Gameboard1.getGameboard());
 
 
-function gameLogic () {
+function gameRound (boardInstance) {
     //for every round u play, get the player's info
     function createPlayer (name) {
         return {
-            name: name,
-            login() {
-                console.log(`${name} has logged in`)
+            name: name
         }
     }
-    const player1 = createPlayer('alice');
-    const player2 = createPlayer('bob');
 
-    //identify which player is playing, add X and O, identify winning condition
-    //switching turns, check if that position is already taken
+    let currentArray = getGameboard() //store the gameboard[] inside another var 
 
-    /*winning conditions:
-        0,1,2
-        3,4,5
-        6,7,8
-        0,3,6
-        1,4,7
-        2,5,8
-        0,4,8
-        2,4,6*/
+    function gameOver () {
+        if (winCondition()) {
+            console.log(gameboard)
+            console.log(`you win`)
+        } else if (!gameboard.includes('')) { //if the array has no empty strings, so no spots left to fill
+            gameboard
+            console.log(`its a tie!`)
+        }
+    }
+
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    return {
+        winCondition: () => {
+            return winningCombos.some(([a, b, c]) => {
+                return gameboard[a] !== '' &&
+                       gameboard[a] === gameboard[b] &&
+                       gameboard[a] === gameboard[c];
+            });
+        },
+        createPlayer,
+        gameOver
+    }
+   
+    //identify which player is playing, add X and O
+    //switching turns, get the board after every turn
+
 }
-}
+
+/*
+const gameLogicInstance = gameRound();
+
+const player1 = gameLogicInstance('alice');
+const player2 = gameLogicInstance('bob');
+console.log(player1) 
+*/
+
