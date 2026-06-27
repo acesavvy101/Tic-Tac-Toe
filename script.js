a637f8fc11f29c3416573e136c62ca6bc950b7d1
acesavvy101 <gracielladeronne07@gmail.com>
2026-06-27 13:17:20 +0800

    feat: change array into fixed positions,disable override, add public getter

--- script.js
+++ script.js
@@ -4,25 +4,35 @@
 3. plauyers are going to be stored in objects not in Gameboard 
 4. in the game logic/ players take turns adding X and O to the array
 5. the array will be appended to the display in displayBoard()
-4. add, reset behaviors objects*/
+4. add, reset behaviors objects
+wrap in IIFE*/
 
 function Gameboard () { 
-    //Gameboard should represent the state of the board at fixed positions
-    let gameboard = [];
+    //instead of pushing into gameboard, we should already have an array with 9 spots, and we need to replace a certain ele in the array (direct indexing)
+    let gameboard = ['','','','','','','','',''];
 
     return { 
-        add: (item) => {
-            if (gameboard.length < 9) {
-                gameboard.push(item) //limit to 9 spots
-            } else {
-                return;
-            }
-        } 
+        add: (label, position) => { 
+        //disable override, check if the position is empty
+          if (gameboard[position]==='') {
+            gameboard[position] = label;
+          } else {
+            console.log(`choose a different position!`)
+          }
+        },
+
         reset: () => {
-            gameboard.length = 0 
-        }
+            gameboard = ['','','','','','','','','']; //make it turn into it's initial state
+        },
+
+        getGameboard: () => gameboard //public getter (maintain encapsulation) | CONSIDER: making it readonly
     }
 }
+//inspect board state
+const Gameboard1 = Gameboard();
+Gameboard1.add('X', 0);
+Gameboard1.add('O',4);
+console.log(Gameboard1.getGameboard());
 
 
 function gameLogic () {
@@ -30,11 +40,24 @@ function gameLogic () {
     function createPlayer (name) {
         return {
             name: name,
-        };
+            login() {
+                console.log(`${name} has logged in`)
+        }
     }
     const player1 = createPlayer('alice');
     const player2 = createPlayer('bob');
 
-    //this is where players take turns adding X and O to the array
-}
+    //identify which player is playing, add X and O, identify winning condition
+    //switching turns, check if that position is already taken
 
+    /*winning conditions:
+        0,1,2
+        3,4,5
+        6,7,8
+        0,3,6
+        1,4,7
+        2,5,8
+        0,4,8
+        2,4,6*/
+}
+}
